516ed8834c8929cad3f1f8f70877b9c8433853f8
DQuentinD <quentindumartheray@gmail.com>
2026-05-11 11:18:34 +0200

    TEST: Suppression du Hello pour voir lechec (section 12)

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello() :
-	return "Hello EPSIC 158 ! - Modifie automatiquement par Jenkins via le webhook GitHub !"
+	return "Goodbye World"
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
